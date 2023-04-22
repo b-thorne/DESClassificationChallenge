3592a346fc7bca1ae60969449f2905d3453e148a
Ben Thorne <bn.thorne@gmail.com>
2023-04-22 11:31:12 -0700

    Added perlmutter batch script

---
+++ perlmutter_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -A m4287
+#SBATCH -C gpu
+#SBATCH -q shared
+#SBATCH -t 2:30:00
+#SBATCH -n 1
+#SBATCH -c 32
+#SBATCH --gpus-per-task=1
+
+export SLURM_CPU_BIND="cores"
+srun python run_experiment.py --DEBUG \
+    --data-dir /global/cfs/cdirs/m4287/cosmology/dessn/stamps \
+    --labels-path /global/cfs/cdirs/m4287/cosmology/dessn/relabeled_autoscan_features.csv \
+    --mode training \
+    --num-workers 32 \
+    --train-length 430000 \
+    --test-length 40000 \
+    --epochs 30
