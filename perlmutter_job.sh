b515126c02c5c077b8b37b9d6b1b95fa3d7847c5
Ben Thorne <bn.thorne@gmail.com>
2023-04-22 13:21:50 -0700

    Add weight decay to job script

--- perlmutter_job.sh
+++ perlmutter_job.sh
@@ -8,11 +8,12 @@
 #SBATCH --gpus-per-task=1
 
 export SLURM_CPU_BIND="cores"
-srun python run_experiment.py --DEBUG \
+srun /global/u1/b/bthorne/miniforge3/envs/fairuniverse/bin/python run_experiment.py --DEBUG \
     --data-dir /global/cfs/cdirs/m4287/cosmology/dessn/stamps \
     --labels-path /global/cfs/cdirs/m4287/cosmology/dessn/relabeled_autoscan_features.csv \
     --mode training \
     --num-workers 32 \
     --train-length 430000 \
     --test-length 40000 \
-    --epochs 30
+    --epochs 30 \
+    --weight-decay
