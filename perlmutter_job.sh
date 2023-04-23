7655d93a4dad6926662a018213e3ec8628cffd4f
Benjamin Thorne <bn.thorne@googlemail.com>
2023-04-23 08:03:21 -0700

    Default to shared queue

--- perlmutter_job.sh
+++ perlmutter_job.sh
@@ -4,17 +4,18 @@
 #SBATCH -q shared
 #SBATCH -t 2:30:00
 #SBATCH -n 1
-#SBATCH -c 32
+#SBATCH -c 128
 #SBATCH --gpus-per-task=1
-#SBATCH -e logs/slurm-%j.out
+#SBATCH --error=logs/slurm-%j.out
 
 export SLURM_CPU_BIND="cores"
 srun /global/u1/b/bthorne/miniforge3/envs/fairuniverse/bin/python run_experiment.py --DEBUG \
     --data-dir /global/cfs/cdirs/m4287/cosmology/dessn/stamps \
     --labels-path /global/cfs/cdirs/m4287/cosmology/dessn/relabeled_autoscan_features.csv \
     --mode training \
-    --num-workers 32 \
+    --num-workers 128 \
     --train-length 430000 \
     --test-length 40000 \
     --epochs 30 \
-    --weight-decay
+    --batch-size 16 \
+    --learning-rate 0.005
