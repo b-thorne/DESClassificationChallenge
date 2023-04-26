5ebcb5ef09429d73e73197958527b3d46d3ce002
Benjamin Thorne <bn.thorne@googlemail.com>
2023-04-25 23:00:54 -0700

    Refactor repository

--- perlmutter_job.sh
+++ perlmutter_job.sh
@@ -4,12 +4,13 @@
 #SBATCH -q shared
 #SBATCH -t 2:30:00
 #SBATCH -n 1
-#SBATCH -c 128
+#SBATCH -c 32
 #SBATCH --gpus-per-task=1
-#SBATCH --error=logs/slurm-%j.out
+#SBATCH -o=logs/slurm-%j.out
 
 export SLURM_CPU_BIND="cores"
-srun /global/u1/b/bthorne/miniforge3/envs/fairuniverse/bin/python run_experiment.py --DEBUG \
+
+podman-hpc run --rm --gpu docker.io/bthorne93/des_snae:latest python run_experiment.py --DEBUG \
     --data-dir /global/cfs/cdirs/m4287/cosmology/dessn/stamps \
     --labels-path /global/cfs/cdirs/m4287/cosmology/dessn/relabeled_autoscan_features.csv \
     --mode training \
