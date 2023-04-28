a15178bfdddda11150ec30cde942fcb47b8ca805
Benjamin Thorne <bn.thorne@googlemail.com>
2023-04-28 15:17:50 -0700

    Make pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fairuniverse",
+    version="0.1.0",
+    description="Cosmology challenge for fairuniverse project",
+    author="Benjamin Thorne",
+    author_email="bthorne@lbl.gov",
+    url="https://github.com/bthorne93/fairuniverse",
+    python_requires=">=3.6",
+)
